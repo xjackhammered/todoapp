dc010764bade3196747f1edbbbcd4e824b35bd8d
xjackhammered <159502632+xjackhammered@users.noreply.github.com>
2024-04-06 14:40:48 +0600

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,36 +1,80 @@
 def main():
-    todos = []
+    task_list = []
+    
     while True:
-        user_input = int(input('''What do you want to do today? 
-                            1. Add task 
-                            2. View List
-                            3. Delete Task
-                            4. Exit
-                                            '''))
-        if user_input == 4:
+        print('''Choose an option
+                    1. Add Task
+                    2. Delete Task
+                    3. View Tasks
+                    4. Clear todo list
+                    5. Exit
+                    ''')
+        n = int(input("Choose an option: "))
+        if n == 1:
+            add_task(task_list)
+        elif n == 2:
+            delete_task()
+        elif n == 3:
+            view_tasks()
+        elif n == 4:
+            clear_list()
+        elif n == 5:
             exit()
+        
+def add_task(task_list):
+    while True:
+        print("Input tasks or press 'x' to exit: ")
+        task = input("Input task: ")
+        if task.upper() == "X":
+            break
         else:
-            choice(user_input, todos)
+            task_list.append(task)
+        with open("todos.txt","w") as file:
+            for tasks in task_list:
+                file.write(tasks + "\n")
 
-def choice(user_input, todos):
-    if user_input == 1:
-        while True:
-            task = input("What task do you want to add? (Press 'x' to stop): ").capitalize()
-            if task == "X":
-                break
-            else:
-                todos.append(task)
-    elif user_input == 2:
-        for task in todos:
-            print("{}. {}".format((todos.index(task))+1, task))
-    elif user_input == 3:
-        delete_task = input("Which task do you want to delete? ").capitalize()
-        del_index = todos.index(delete_task)
-        todos.pop(del_index)
-    return todos
+
+def delete_task():
+
+    del_task = input("Type the name of the task that you want to delete: ").strip().capitalize()
+
+    with open("todos.txt", "r") as file:
+        content = file.readlines()
+
+    found = False
+    for i in range(len(content)):
+        task = content[i].strip()
+
+        if task.capitalize() == del_task:
+            found = True
+            del content[i]
+            print("Deleted Successfully!")
+            break
+
+    if not found:
+        print("Task not found!")
+        return
+
+    with open("todos.txt", "w") as file:
+        for task in content:
+            file.write(task)
+
+def clear_list():
+    with open("todos.txt","r") as file:
+        content = file.readlines()
+
+    with open("todos.txt","w") as file:
+        content.clear()
+        for i in content:
+            file.write(i)
+    
+    print("List is cleared successfully!")
+
+def view_tasks():
+    with open("todos.txt","r") as file:
+        read_content = file.readlines()
+        for i in read_content:
+            print("{}.{}".format(read_content.index(i)+1,i.rstrip().capitalize()))
+
 
 main()
-
-
-
-        
